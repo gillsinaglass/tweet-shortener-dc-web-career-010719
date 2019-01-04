b7fe840a7cbb6ae735eb05412cbf52cb4a5b6d1b
Ryan Gill <rdgill95@gmail.com>
2019-01-04 13:20:32 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -32,7 +32,7 @@ end
 def selective_tweet_shortener(tweet)
   print_variable = 140
   if tweet.chars.length > print_variable
-    tweet.each do |tweets|
+   tweet.each do |tweets| 
      puts word_substituter(tweets)
    end
  else
